f934f5019a44258729421d6a74a0956e8d41b86f
PirleaAndrei <pirleaandrei1@yahoo.com>
2022-06-19 13:05:28 +0300

    v1.2

--- password generator/index.js
+++ password generator/index.js
@@ -3,8 +3,8 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&",
 "*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let password1=document.querySelector("#first-Part")
-let password2=document.querySelector("#second-Part")
+let password1=document.querySelector("#firstPart")
+let password2=document.querySelector("#secondPart")
 function generate(){
    password1.textContent=""
    password2.textContent=""
